2d075b62d9542c154a76648a0a8c04b2cad34427
Phillip <philsher93@gmail.com>
2022-06-24 23:31:43 -0400

    Add gif

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src="https://i.gifer.com/3hHq.gif" alt="spirited-away" />
         <a
           className="App-link"
           href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
           target="_blank"
           rel="noopener noreferrer"
         >
-          {`Click me! Coolest link ever`}
+          {`Click me!`}
         </a>
       </header>
     </div>
